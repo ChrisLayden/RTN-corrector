0fe3fe95a8d231dc7cefb6c9e9d3e023e16527ee
Christopher Layden <clayden7@mit.edu>
2026-01-02 11:30:52 -0500

    fit_testing notebook for Tejus

--- rtn_fitter.py
+++ rtn_fitter.py
@@ -50,9 +50,11 @@ def get_read_noise_stats(data, adu_unit, plot=False):
     mean_read_noise = np.nanmean(read_noise_array)
     median_read_noise = np.nanmedian(read_noise_array)
     rms_read_noise = np.sqrt(np.nanmean(read_var_array))
+    read_noise_sigma = np.nanstd(read_noise_array, ddof=1)
     read_noise_stats = {"mean_read_noise": mean_read_noise,
                         "median_read_noise": median_read_noise,
-                        "rms_read_noise": rms_read_noise}
+                        "rms_read_noise": rms_read_noise,
+                        "read_noise_sigma": read_noise_sigma}
     if plot:
         plt.hist(read_noise_array.flatten(), bins=200, histtype='step', alpha=0.7)
         plt.xlabel("Read Noise (e-)" if gain is not None else "Read Noise (ADU)")
@@ -91,20 +93,23 @@ def ad_statistics_normal(data):
     A2 = -n - np.mean(term, axis=0)  # shape: (nx, ny)
     return A2
 
-def identify_nonnormal_pixels(data, threshold=1.092, plot=False):
+def identify_nonnormal_pixels(data, adu_unit, threshold=1.092, plot=False):
     # Identify pixels with non-normal distributions based on AD statistic.
     # Threshold of 1.092 corresponds to 1% significance level for normality test
     # with infinite samples and unknown mean and variance (see https://doi.org/10.2307/2286009)
     A2 = ad_statistics_normal(data)
-    nonnormal_mask = A2 > threshold
+    # read_noise_array = np.sqrt(np.var(data, axis=0, ddof=1))
+    read_noise_stats, read_noise_array = get_read_noise_stats(data, adu_unit, plot=False)
+    # Don't try to correct pixels with read noise already close to the median
+    low_cutoff = read_noise_stats["median_read_noise"]
+    nonnormal_mask = (A2 > threshold) & (read_noise_array > low_cutoff)
     # Plot read noise histogram of non-normal pixels
     if plot:
-        read_var_array = np.var(data, axis=0, ddof=1)
-        read_noise_array = np.sqrt(read_var_array)
+        read_noise_array = read_noise_array.to(u.electron).value
         nonnormal_read_noise = read_noise_array[nonnormal_mask]
         plt.hist(read_noise_array.flatten(), bins=200, range=(0, np.max(read_noise_array)), histtype='step', alpha=0.3, label='All Pixels')
         plt.hist(nonnormal_read_noise.flatten(), bins=200, range=(0, np.max(read_noise_array)), histtype='step', alpha=0.7, label='Pixels failing AD test')
-        plt.xlabel("Read Noise (ADU)")
+        plt.xlabel("Read Noise (e-)")
         plt.ylabel("Number of Non-normal Pixels")
         plt.yscale('log')
         plt.title("Read Noise Distribution of Non-normal Pixels")
@@ -114,9 +119,9 @@ def identify_nonnormal_pixels(data, threshold=1.092, plot=False):
 
 def rtn_triple_gaussian(x, mu, A, B1, B2, d, sigma):
     # Model RTN pixel histogram as sum of three Gaussians.
-    central_gaussian = A * np.exp(- (x - mu) ** 2 / (2 * sigma) ** 2)
-    left_gaussian = B1 * np.exp(- (x - d - mu) ** 2 / (2 * sigma) ** 2)
-    right_gaussian = B2 * np.exp(- (x + d - mu) ** 2 / (2 * sigma) ** 2)
+    central_gaussian = A * np.exp(- (x - mu) ** 2 / sigma ** 2 / 2)
+    left_gaussian = B1 * np.exp(- (x - d - mu) ** 2 / sigma ** 2 / 2)
+    right_gaussian = B2 * np.exp(- (x + d - mu) ** 2 / sigma ** 2 / 2)
     return central_gaussian + left_gaussian + right_gaussian
 
 def fit_rtn_parameters(data, nonnormal_mask, read_noise_stats, adu_unit, min_spacing=3):
@@ -134,7 +139,7 @@ def fit_rtn_parameters(data, nonnormal_mask, read_noise_stats, adu_unit, min_spa
         Unit representing ADU (Analog-to-Digital Units).
     min_spacing : float, optional
         Minimum spacing between central and side peaks that can be corrected,
-        in multiples of central peak standard deviation.
+        in multiples of the noise at 1 e-/frame. Default is 3.
     
     Returns:
     --------
@@ -164,11 +169,11 @@ def fit_rtn_parameters(data, nonnormal_mask, read_noise_stats, adu_unit, min_spa
             B1_guess = 0.1 * A_guess
             B2_guess = 0.1 * A_guess
             d_guess = (10 * u.electron).to(adu_unit).value  # Initial guess of 10 e- spacing
-            d_min = 3 * sigma_guess
             p0 = [mu_guess, A_guess, B1_guess, B2_guess, d_guess, sigma_guess]
+            bounds = ([0, 0, 0, 0, 0, 0], [np.inf, np.inf, A_guess, A_guess, np.inf, np.inf])
             try:
-                popt, pcov = curve_fit(rtn_triple_gaussian, bin_centers, counts, p0=p0, maxfev=1000, 
-                                    bounds=([0, 0.5 * A_guess, 0, 0, d_min, 0], [np.inf, np.inf, A_guess, A_guess, np.inf, np.inf]))
+                popt, pcov = curve_fit(rtn_triple_gaussian, bin_centers, counts, p0=p0,
+                                       maxfev=1000, bounds=bounds, full_output=False)
                 mu_fit = popt[0]
                 A_fit = popt[1] / (popt[1] + popt[2] + popt[3])  # Normalize A
                 B_fit = popt[3] / (popt[1] + popt[2] + popt[3])  # Normalize B1
@@ -178,18 +183,23 @@ def fit_rtn_parameters(data, nonnormal_mask, read_noise_stats, adu_unit, min_spa
                 errs = np.sqrt(np.diag(pcov))
                 if np.any(popt / errs < 3):
                     poor_fit_count += 1
+                    # print(mu_fit, A_fit, B_fit, d_fit / 42, sigma_fit / 42, np.std(pixel_data) / 42)
+                    # plt.hist(pixel_data, bins=50, density=True, alpha=0.6, label='Data Histogram')
+                    # x_fit = np.linspace(np.min(bin_centers), np.max(bin_centers), 200)
+                    # y_fit = rtn_triple_gaussian(x_fit, *popt)
+                    # plt.plot(x_fit, y_fit, 'r-', label='Fit')
+                    # plt.show()
                 # Check if spacing is sufficient for correction when photon flux is 1 e-/frame
                 noise_at_1e = np.sqrt(sigma_fit ** 2 + (1 * u.electron).to(adu_unit).value ** 2)
                 if (d_fit >= min_spacing * noise_at_1e) and A_fit < 0.95:
                     rtn_params[(ix, iy)] = (mu_fit * adu_unit, A_fit, B_fit, d_fit * adu_unit, sigma_fit * adu_unit)
                     rtn_mask[ix, iy] = True
-                    # if sigma_fit < 42:
-                    #     print(mu_fit, A_fit, B_fit, d_fit / 42, sigma_fit / 42, np.std(pixel_data) / 42)
-                    #     plt.hist(pixel_data, bins=50, density=True, alpha=0.6, label='Data Histogram')
-                    #     x_fit = np.linspace(np.min(bin_centers), np.max(bin_centers), 200)
-                    #     y_fit = rtn_triple_gaussian(x_fit, *popt)
-                    #     plt.plot(x_fit, y_fit, 'r-', label='Fit')
-                    #     plt.show()
+                    # print(mu_fit, A_fit, B_fit, (d_fit * adu_unit).to(u.electron), (sigma_fit * adu_unit).to(u.electron), (np.std(pixel_data) * adu_unit).to(u.electron))
+                    # plt.hist(pixel_data, bins=50, density=True, alpha=0.6, label='Data Histogram')
+                    # x_fit = np.linspace(np.min(bin_centers), np.max(bin_centers), 200)
+                    # y_fit = rtn_triple_gaussian(x_fit, *popt)
+                    # plt.plot(x_fit, y_fit, 'r-', label='Fit')
+                    # plt.show()
                 else:
                     too_close_count += 1
             except RuntimeError:
@@ -225,8 +235,12 @@ def get_lambda_max_arr(rtn_params, threshold=3, plot=False):
         lambda_max = (d / threshold) ** 2 - sigma ** 2
         lambda_max_arr[i] = max(lambda_max, 0) # Ensure non-negative
     if plot:
+        plt.rcParams.update({'font.size': 14})
         plt.hist(lambda_max_arr, bins=100, histtype='step', alpha=0.7)
-        plt.xlabel("Maximum Correctable Electron Flux (e-/frame)")
+        plt.xlabel("Maximum Correctable Electron Flux (e-/pix/frame)")
+        median_lambda_max = np.median(lambda_max_arr)
+        plt.axvline(median_lambda_max, color='red', linestyle='dashed', linewidth=1, label=f"Median: {median_lambda_max:.2f} e-/pix/frame")
+        plt.legend()
         plt.ylabel("Number of RTN Pixels")
         plt.yscale('log')
         plt.show()
@@ -245,9 +259,9 @@ def get_new_read_noise_stats(bias_stack, rtn_params, adu_unit):
                  "rms_read_noise": new_rms}
     return new_stats, corr_read_noise_array
 
-def plot_snr(lambda_max_arr, old_stats, new_stats):
+def plot_snr_ratio(lambda_max_arr, old_stats, new_stats):
     # For increasing electron flux, plot SNR of RTN correction for each pixel.
-    flux_values = np.linspace(0.1, 20, 100)  # e-/frame
+    flux_values = np.linspace(0.1, 30, 300)  # e-/pix/frame
     snr_values_old = np.zeros_like(flux_values)
     snr_values_corr = np.zeros_like(flux_values)
     old_rms = old_stats["rms_read_noise"].to(u.electron).value
@@ -258,19 +272,20 @@ def plot_snr(lambda_max_arr, old_stats, new_stats):
         effective_rms = (1 - frac_correctable) * old_rms + frac_correctable * new_rms
         snr_values_old[i] = lam / np.sqrt(lam + old_rms**2)
         snr_values_corr[i] = lam / np.sqrt(lam + effective_rms**2)
-    plt.plot(flux_values, snr_values_old, label='Effective SNR before RTN Correction')
-    plt.plot(flux_values, snr_values_corr, label='Effective SNR after RTN Correction')
-    plt.xlabel("Electron Flux (e-/frame)")
-    plt.ylabel("Signal-to-Noise Ratio (SNR)")
+    plt.rcParams.update({'font.size': 14})
+    plt.plot(flux_values, snr_values_corr / snr_values_old)
+    plt.xlabel("Electron Flux (e-/pix/frame)")
+    plt.ylabel("Relative SNR Improvement")
+    # Put y grid lines
+    plt.grid(axis='y')
     plt.xscale('log')
-    plt.yscale('log')
-    plt.legend()
     plt.show()
 
 def plot_naive_corrected(data, rtn_params, adu_unit):
     # Plot histogram of read noise before correction and the best possible correction.
     old_stats, read_noise_array = get_read_noise_stats(data, adu_unit)
     new_stats, corr_read_noise_array = get_new_read_noise_stats(data, rtn_params, adu_unit)
+    plt.rcParams.update({'font.size': 14})
     plt.hist(read_noise_array.flatten().to(u.electron).value, bins=200, histtype='step', alpha=0.7, label='Before Correction')
     plt.hist(corr_read_noise_array.flatten().to(u.electron).value, bins=200, histtype='step', alpha=0.7, label='Best-case Correction')
     plt.axvline(old_stats["median_read_noise"].to(u.electron).value, color='red', linestyle='dashed', linewidth=1, label=f"Old Median: {old_stats['median_read_noise']:.2f}")
@@ -283,21 +298,20 @@ def plot_naive_corrected(data, rtn_params, adu_unit):
     plt.show()
     return
 
-
 if __name__ == "__main__":
     t0 = time.time()
     bias_stack_file = 'bias_stack_subset.fits'
+    gain = 42  # ADU/e-
     bias_stack = fits.open(bias_stack_file)[0].data.astype(np.int32)
     t1 = time.time()
     print(f"Loaded bias stack in {t1 - t0:.2f} seconds.")
     # Not sigma clipping for now. Doesn't do much and introduces NaNs.
     # bias_stack = sigma_clip(bias_stack, sigma=10, max_iters=5)
-    gain = 42  # ADU/e-
     adu = u.electron / gain
-    read_noise_stats, read_noise_array = get_read_noise_stats(bias_stack, adu, plot=False)
+    read_noise_stats, read_noise_array = get_read_noise_stats(bias_stack, adu, plot=True)
     t2 = time.time()
     print(f"Computed read noise stats in {t2 - t1:.2f} seconds.")
-    nonnormal_mask = identify_nonnormal_pixels(bias_stack, plot=False)
+    nonnormal_mask = identify_nonnormal_pixels(bias_stack, adu, plot=True)
     t3 = time.time()
     print(f"Identified non-normal pixels in {t3 - t2:.2f} seconds.")
     rtn_params, rtn_mask = fit_rtn_parameters(bias_stack, nonnormal_mask, read_noise_stats, adu, min_spacing=3)
@@ -320,4 +334,4 @@ if __name__ == "__main__":
     lambda_max_arr = get_lambda_max_arr(rtn_params, threshold=3.5, plot=True)
     plot_naive_corrected(bias_stack, rtn_params, adu)
     corr_stats, corr_read_noise_array = get_new_read_noise_stats(bias_stack, rtn_params, adu)
-    plot_snr(lambda_max_arr, read_noise_stats, corr_stats)
+    plot_snr_ratio(lambda_max_arr, read_noise_stats, corr_stats)
